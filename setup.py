1f920edfc7797bbe5c9f9852f958a417b2d040bd
Mikhail Krassavin <mikhail.krassavin@gmail.com>
2022-08-12 00:45:59 +0600

    update python version in the description

--- setup.py
+++ setup.py
@@ -27,12 +27,9 @@ setup(
         'Natural Language :: English',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: System :: Installation/Setup'
     ]
 )
